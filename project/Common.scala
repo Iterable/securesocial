8f361e1e95634d80c345f4f8cb44e04f951094bc
Greg Methvin <greg@methvin.net>
2019-11-05 16:42:41 -0800

    increment version

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@
 
 object Common {
   def organization = "com.iterable"
-  def version = "4.0.0-M2"
+  def version = "4.0.0-M3"
   def playVersion = System.getProperty("play.version", "2.7.3")
   def scalaVersion = System.getProperty("scala.version", "2.12.8")
   def crossScalaVersions = Seq(scalaVersion, "2.11.12")
