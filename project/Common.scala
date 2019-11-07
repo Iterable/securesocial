12fece31a629021450d119a63218dc829ccc58b9
Ilya Brin <ilya@iterable.com>
2019-11-07 14:14:45 -0800

    bump version to M12

--- project/Common.scala
+++ project/Common.scala
@@ -2,7 +2,7 @@
 
 object Common {
   def organization = "com.iterable"
-  def version = "3.0.0-M11"
+  def version = "3.0.0-M12"
   def playVersion = System.getProperty("play.version", "2.6.12")
   def scalaVersion = System.getProperty("scala.version", "2.12.6")
   def crossScalaVersions = Seq(scalaVersion, "2.11.12")
